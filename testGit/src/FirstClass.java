ee665fe4889b2309c36b5bd7056c2f5550e60568
wbq <1284611204@qq.com>
2018-07-17 19:52:38 +0800

    fgvdfg

--- testGit/src/FirstClass.java
+++ testGit/src/FirstClass.java
@@ -2,7 +2,7 @@ public class FirstClass {
     public static void main(String[] args) {
         System.out.println("aaaaa");
         System.out.println("testpush");
-        System.out.println("testmerge1");
+        System.out.println("testmerge2");
     }
     int i = 0;
 }
